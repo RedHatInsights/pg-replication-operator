f6fc194b247a340816e184a3a8d62ac7fe82fe1c
Viliam Krizan <vkrizan@redhat.com>
2025-02-04 15:48:59 +0100

    feat: introduce LogicalReplication API

--- cmd/main.go
+++ cmd/main.go
@@ -18,6 +18,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	replicationv1alpha1 "github.com/RedHatInsights/pg-replication-operator/api/v1alpha1"
+	"github.com/RedHatInsights/pg-replication-operator/internal/controller"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -29,6 +32,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(replicationv1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -124,6 +128,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.LogicalReplicationReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "LogicalReplication")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
