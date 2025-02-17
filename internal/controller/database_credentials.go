6aba5e58d7dbcdb84701e4cdcf8504c3ec21c1f2
Viliam Krizan <vkrizan@redhat.com>
2025-02-17 13:16:42 +0100

    feat: getCredentialsFromSecret

---
+++ internal/controller/database_credentials.go
@@ -0,0 +1,24 @@
+package controller
+
+type DatabaseCredentials struct {
+	// Hostname
+	Host string `mapstructure:"db.host"`
+
+	// Port
+	Port string `mapstructure:"db.port"`
+
+	// Regular username
+	User string `mapstructure:"db.user"`
+
+	// Password for the regular user
+	Password string `mapstructure:"db.password"`
+
+	// Name of the database
+	DatabaseName string `mapstructure:"db.name"`
+
+	// Username of the admin account
+	AdminUser string `mapstructure:"db.admin_user"`
+
+	// Password of the admin account
+	AdminPassword string `mapstructure:"db.admin_password"`
+}
