a138a4074c9c72a39d2d4bb04c10ab97a942c9a8
Michael Mraka <michael.mraka@redhat.com>
2025-03-05 10:25:29 +0100

    feat: check publication

--- internal/replication/connect.go
+++ internal/replication/connect.go
@@ -17,3 +17,36 @@ func DBConnect(credentials DatabaseCredentials) (*sql.DB, error) {
 	err = db.Ping()
 	return db, err
 }
+
+func CheckPublication(db *sql.DB, name string) error {
+	row := db.QueryRow(`SELECT p.puballtables,
+							   (p.pubinsert AND p.pubupdate AND p.pubdelete AND p.pubtruncate) as pubops,
+							   (SELECT COUNT(*) FROM pg_publication_namespace pn WHERE p.oid = pn.pnpubid) as pubnamespaces
+						  FROM pg_publication p
+						 WHERE p.pubname = $1`, name)
+	var (
+		puballtables  bool
+		pubops        bool
+		pubnamespaces int
+	)
+
+	err := row.Scan(&puballtables, &pubops, &pubnamespaces)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			err = fmt.Errorf("publication '%s' does not exist", name)
+		}
+		return err
+	}
+
+	// publication should not be FOR ALL TABLES
+	// allow insert/update/delete/truncate
+	// should not be FOR TABLES IN SCHEMA
+	if puballtables ||
+		!pubops ||
+		pubnamespaces > 0 {
+		err := fmt.Errorf("publication '%s' has wrong attributes", name)
+		return err
+	}
+
+	return nil
+}
