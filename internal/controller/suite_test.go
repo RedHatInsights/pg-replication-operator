6479cf20fa68a1eae6d99787a4ad4459b38447d0
Michael Mraka <michael.mraka@redhat.com>
2025-03-14 17:04:10 +0100

    test(subscription): setup postgres container and db

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -51,7 +51,7 @@ func setupPgContainer(ctx context.Context) (*postgres.PostgresContainer, replica
 		"docker.io/library/postgres:16-alpine",
 		testcontainers.CustomizeRequest(testcontainers.GenericContainerRequest{
 			ContainerRequest: testcontainers.ContainerRequest{
-				Cmd: []string{"-c", "wal_level=logical"},
+				Cmd: []string{"-c", "wal_level=logical", "-c", "listen_addresses=*"},
 			},
 		}),
 		postgres.WithInitScripts(filepath.Join("..", "..", "test", "data", "create_databases.sql")),
@@ -67,12 +67,24 @@ func setupPgContainer(ctx context.Context) (*postgres.PostgresContainer, replica
 	Expect(err).ToNot(HaveOccurred())
 	port, err := pgContainer.MappedPort(ctx, "5432")
 	Expect(err).ToNot(HaveOccurred())
+	portStr := port.Port()
 	pgCredentials := replication.DatabaseCredentials{
 		Host:          host,
-		Port:          port.Port(),
+		Port:          portStr,
 		AdminUser:     "postgres",
 		AdminPassword: "postgres",
 	}
+
+	// we need the same port for postgres both externaly and internaly
+	_, _, err = pgContainer.Exec(ctx, []string{"sh", "-c", "nc -lkp" + portStr + " -e nc localhost 5432 &"})
+	Expect(err).Should(BeNil())
+
+	// finish subscription
+	_, _, err = pgContainer.Exec(ctx, []string{"psql", "-U", "postgres", "-d", "subscriber_db",
+		"-c", "ALTER SUBSCRIPTION publication_v1 CONNECTION 'host=localhost port=" + portStr + " user=publisher_user password=publisher_password dbname=publisher_db sslmode=disable';",
+		"-c", "ALTER SUBSCRIPTION publication_v1 ENABLE;",
+		"-c", "ALTER SUBSCRIPTION publication_v1 REFRESH PUBLICATION;"})
+	Expect(err).Should(BeNil())
 
 	return pgContainer, pgCredentials
 }
