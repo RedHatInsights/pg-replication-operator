826c482b35b084c54c0912a4794e8bb13a071a10
Michael Mraka <michael.mraka@redhat.com>
2025-03-14 17:04:10 +0100

    test(subscription): setup postgres container and db

--- test/data/create_databases.sql
+++ test/data/create_databases.sql
@@ -1,12 +1,14 @@
 CREATE DATABASE publisher_db;
 CREATE USER publisher_user WITH PASSWORD 'publisher_password';
 GRANT ALL PRIVILEGES ON DATABASE publisher_db TO publisher_user;
+ALTER ROLE publisher_user WITH REPLICATION;
 
 CREATE DATABASE subscriber_db;
 CREATE USER subscriber_user WITH PASSWORD 'subscriber_password';
 GRANT ALL PRIVILEGES ON DATABASE subscriber_db TO subscriber_user;
+GRANT pg_create_subscription TO subscriber_user;
 
-\c publisher_db
+\c publisher_db publisher_user
 CREATE SCHEMA IF NOT EXISTS published_data;
 CREATE TABLE IF NOT EXISTS published_data.people
   (id UUID PRIMARY KEY, name VARCHAR(255), email VARCHAR(255) UNIQUE, birthyear INT);
@@ -25,3 +27,22 @@ INSERT INTO published_data.cities VALUES
 CREATE PUBLICATION publication_v1;
 ALTER PUBLICATION publication_v1 ADD TABLE published_data.people (id, name);
 ALTER PUBLICATION publication_v1 ADD TABLE published_data.cities (id, name, zip, country);
+
+\c subscriber_db postgres
+GRANT SELECT ON pg_subscription TO subscriber_user;
+
+\c subscriber_db subscriber_user
+CREATE SCHEMA IF NOT EXISTS published_data;
+CREATE TABLE IF NOT EXISTS published_data.people
+  (id UUID PRIMARY KEY, name VARCHAR(255));
+CREATE TABLE IF NOT EXISTS published_data.cities
+  (id UUID PRIMARY KEY, name VARCHAR(255) UNIQUE, zip VARCHAR(255), country VARCHAR(255));
+
+-- when running publisher and subscriber on the same cluster slot has to be created manually
+CREATE SUBSCRIPTION publication_v1
+  CONNECTION 'password=null'
+  PUBLICATION publication_v1
+  WITH (connect=false);
+
+\c publisher_db publisher_user
+SELECT pg_create_logical_replication_slot('publication_v1', 'pgoutput');
